25e1d3f99ca4778ff162c4a9fead9f3fd365e4d8
Nick O'Connell <business@nickoconnell.dev>
2025-08-03 02:19:23 -0500

    Fix SampleScene

--- Assets/Events/NarrativeSystem.cs
+++ Assets/Events/NarrativeSystem.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class NarrativeSystem : MonoBehaviour
 {
     public NarrativeEvent narrativeEvent;
-    public Interactable interactable;
+    private Interactable interactable;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
