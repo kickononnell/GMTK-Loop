bda6f20f88abd7ca698422a92a94abd57dc0bfb2
jcreeno77 <jcreeno77@gmail.com>
2025-08-03 04:49:20 -0500

    changoos
    JC Changarooooosss

--- Assets/Scripts/ManageLoopVideo.cs
+++ Assets/Scripts/ManageLoopVideo.cs
@@ -11,24 +11,29 @@ public class ManageLoopVideo : MonoBehaviour
     [SerializeField] GameObject HouseDestroyVideoObj;
 
     [SerializeField] float AccelerateOpacitySpeed = 2;
+    [SerializeField] AudioSource vidAudSrc;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        //StartCoroutine(PlayEndVid());
     }
 
     public IEnumerator PlayEndVid()
     {
         HouseDestroyVideoObj.SetActive(true);
         vidImage.color = new Color(1, 1, 1, 0);
+        vidAudSrc.Play();
+        vidP.Play();
+        vidP.frame = 0;
         while (vidImage.color.a < 1)
         {
             vidImage.color += new Color(0, 0, 0, Time.deltaTime* AccelerateOpacitySpeed);
             yield return null;
         }
-        vidP.Play();
+        
 
-        yield return new WaitForSeconds((float)vidP.clip.length);
+        yield return new WaitForSeconds((float)vidP.clip.length/5);
+        HouseDestroyVideoObj.SetActive(false);
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
